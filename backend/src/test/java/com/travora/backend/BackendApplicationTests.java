1790b717925acd9aa625bdcf954e4a99fbbbaa69
Omar Fayaz <omarfayaz.business@gmail.com>
2026-03-17 13:00:51 +0800

    Set up Spring Boot project with dependencies

---
+++ backend/src/test/java/com/travora/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.travora.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
