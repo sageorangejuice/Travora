76b8dc2a78adccc0217a3d1309b04c2c6b7460e0
Omar Fayaz <omarfayaz.business@gmail.com>
2026-03-22 13:34:33 +0800

    Added LX's login code

--- backend/src/main/java/com/travora/backend/model/User.java
+++ backend/src/main/java/com/travora/backend/model/User.java
@@ -16,10 +16,39 @@ public class User {
     private String username;
     private String password;
 
-    public String getUsername() { 
+    public String getUsernameFromDatabase() { 
         return username; 
     }
-    public String getPassword() { 
+    public String getPasswordFromDatabase() { 
         return password;
     }
 }
+
+//LIXIANG'S CODE --> TO USE SOON
+// package com.travora.backend.model;
+
+// import jakarta.persistence.*;
+
+// @Entity
+// @Table(name = "user_credentials")
+// public class User {
+
+//     @Id
+//     @GeneratedValue(strategy = GenerationType.IDENTITY)
+//     private Long id;
+
+//     @Column(name = "username", nullable = false, unique = true)
+//     private String username;
+
+//     @Column(name = "password", nullable = false)
+//     private String password;
+
+// // getters n setters
+//     public Long getId() { return id; }
+
+//     public String getUsername() { return username; }
+//     public void setUsername(String username) { this.username = username; }
+
+//     public String getPassword() { return password; }
+//     public void setPassword(String password) { this.password = password; }
+// }
